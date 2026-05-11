ae85629550d9885b9990df90dfdad04a28e272ec
Diane <diane.ngo1321@gmail.com>
2026-05-11 21:17:09 +0000

    update pubsub for custom message

--- src/py_pubsub/py_pubsub/publisher_member_function.py
+++ src/py_pubsub/py_pubsub/publisher_member_function.py
@@ -16,23 +16,23 @@ import rclpy
 from rclpy.executors import ExternalShutdownException
 from rclpy.node import Node
 
-from std_msgs.msg import String
+from tutorial_interfaces.msg import Num                            # CHANGE
 
 
 class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.publisher_ = self.create_publisher(Num, 'topic', 10)  # CHANGE
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        msg = Num()                                                # CHANGE
+        msg.num = self.i                                           # CHANGE
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.get_logger().info('Publishing: "%d"' % msg.num)       # CHANGE
         self.i += 1
 
 
