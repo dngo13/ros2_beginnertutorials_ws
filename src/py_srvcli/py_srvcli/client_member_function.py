9aec41ddfb66448314f4c169bc850b95723e0ed1
Diane <diane.ngo1321@gmail.com>
2026-05-12 15:25:34 +0000

    update python action server client

--- src/py_srvcli/py_srvcli/client_member_function.py
+++ src/py_srvcli/py_srvcli/client_member_function.py
@@ -1,4 +1,4 @@
-from example_interfaces.srv import AddTwoInts
+from tutorial_interfaces.srv import AddThreeInts                            # CHANGE
 
 import rclpy
 from rclpy.executors import ExternalShutdownException
@@ -9,14 +9,15 @@ class MinimalClientAsync(Node):
 
     def __init__(self):
         super().__init__('minimal_client_async')
-        self.cli = self.create_client(AddTwoInts, 'add_two_ints')
+        self.cli = self.create_client(AddThreeInts, 'add_three_ints')       # CHANGE
         while not self.cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('service not available, waiting again...')
-        self.req = AddTwoInts.Request()
+        self.req = AddThreeInts.Request()                                   # CHANGE
 
     def send_request(self):
         self.req.a = 41
         self.req.b = 1
+        self.req.c = 1                                                      # CHANGE
         return self.cli.call_async(self.req)
 
 
@@ -28,8 +29,8 @@ def main(args=None):
             rclpy.spin_until_future_complete(minimal_client, future)
             response = future.result()
             minimal_client.get_logger().info(
-                'Result of add_two_ints: for %d + %d = %d' %
-                (minimal_client.req.a, minimal_client.req.b, response.sum))
+                'Result of add_three_ints: for %d + %d + %d = %d' %                                # CHANGE
+                (minimal_client.req.a, minimal_client.req.b, minimal_client.req.c, response.sum))  # CHANGE
     except (KeyboardInterrupt, ExternalShutdownException):
         pass
 
